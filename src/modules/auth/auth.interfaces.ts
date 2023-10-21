519accfa8a310d178be2b905fd2f1c9de9c9d4c1
Stanislav <verbatim8872@gmail.com>
2023-10-22 00:44:52 +0300

    cleanup

--- src/modules/auth/auth.interfaces.ts
+++ src/modules/auth/auth.interfaces.ts
@@ -4,11 +4,3 @@ export interface ITokenPayload {
   username: string;
   sub: Schema.Types.ObjectId;
 }
-
-export interface IUser {
-  id: Schema.Types.ObjectId;
-  login: string;
-  firstName: string;
-  lastName: string;
-  pets: string[];
-}
